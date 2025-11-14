a0d9e5fc73f83da9ec47098076784ede71fa0078
jocim-nix <joe.susatiyo@gmail.com>
2025-11-14 23:10:44 +0700

    testing watchtower

--- src/routes/hackathon.js
+++ src/routes/hackathon.js
@@ -5,7 +5,7 @@ const router = Router();
 
 router.get("/", async (req, res) => {
   check(req, res);
-  res.json({ msg: "Hello World" });
+  res.json({ msg: "We do a little trolling" });
 });
 
 export default router;
